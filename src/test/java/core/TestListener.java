ca92bb1775773b0288ded2cd0818f72231536d26
heru-cmu <herupenyu@gmail.com>
2026-03-12 08:17:33 +0700

    tahap 2 penambahan report pakai allure yg alot krn kompabilitas versi gradle dan jdk pembuatan listener dan log juga variasi report

--- src/test/java/core/TestListener.java
+++ src/test/java/core/TestListener.java
@@ -1,4 +1,42 @@
 package core;
 
-public class TestListener {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TestListener implements ITestListener {
+    private static final Logger logger = LogManager.getLogger(TestListener.class);
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        logger.info("=== STARTING TEST: " + result.getName() + " ===");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        logger.info("=== PASSED TEST: " + result.getName() + " ===");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        logger.error("=== FAILED TEST: " + result.getName() + " ===");
+        TestUtils.takeScreenshot(DriverManager.getDriver(), result.getName());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        logger.warn("=== SKIPPED TEST: " + result.getName() + " ===");
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        logger.info("=== STARTING SUITE: " + context.getName() + " ===");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        logger.info("=== FINISHED SUITE: " + context.getName() + " ===");
+    }
 }
--- src/test/java/core/TestUtils.java
+++ src/test/java/core/TestUtils.java
@@ -1,4 +1,29 @@
 package core;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class TestUtils {
+
+    public static void takeScreenshot(WebDriver driver, String testName) {
+        try {
+            File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String destPath = "screenshots/" + testName + ".png";
+            Files.createDirectories(Paths.get("screenshots"));
+            Files.copy(srcFile.toPath(), Paths.get(destPath));
+            System.out.println("Screenshot saved: " + destPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String generateRandomEmail() {
+        return "user" + System.currentTimeMillis() + "@example.com";
+    }
 }
